386e26a26a910022f64dbea2c6dcf4216b4b04de
zainabzahid711 <zainabzahid711@gmail.com>
2023-12-24 21:27:54 +0500

    solve using dutchFlagAlgorithem

--- DsaSheet/Arrays/sort0s1sand2s.cpp
+++ DsaSheet/Arrays/sort0s1sand2s.cpp
@@ -2,19 +2,62 @@
 using namespace std;
 
 // sortt array contains zeros ones and twos
+// without any algorithem
 
-void arraySort(int arr[], int size)
+// first approach is counting approach
+// void sort012(int arr[], int size)
+// {
+//   int count0 = 0, count1 = 0, count2 = 0;
+//   for (int i = 0; i < size; i++)
+//   {
+//     switch (arr[i])
+//     {
+//     case 0:
+//       count0++;
+//       break;
+//     case 1:
+//       count1++;
+//       break;
+//     case 2:
+//       count2++;
+//       break;
+//     }
+//   }
+//   for (int i = 0; i < count0; ++i)
+//   {
+//     arr[i] = 0;
+//   }
+//   for (int i = count0; i < count0 + count1; ++i)
+//   {
+//     arr[i] = 1;
+//   }
+//   for (int i = count0 + count1; i < count0 + count1 + count2; ++i)
+//   {
+//     arr[i] = 2;
+//   }
+// }
+
+// dutch flag algorithem is more efficient approach
+// as both functions sort012 and dutchFlagAlgo has same time complexity O(n)
+void dutchFlagAlgo(int arr[], int size)
 {
-  for (int i = 0; i < size - 1; i++)
+  int mid = 0, low = 0, high = size - 1;
+  while (mid <= high)
   {
-    for (int j = 0; j < size - 1 - i; j++)
+    if (arr[mid] == 0)
     {
-      if (arr[j] > arr[j + 1])
-      {
-        int temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
+      swap(arr[mid], arr[low]);
+      mid++;
+      low++;
+    }
+    else if (arr[mid] == 1)
+    {
+      mid++;
+    }
+    else
+    { // arr[mid]==2
+      swap(arr[mid], arr[high]);
+      high--;
     }
   }
 }
@@ -38,6 +81,7 @@ int main()
   {
     cin >> arry[i];
   }
-  arraySort(arry, size);
+  // sort012(arry, size);
+  dutchFlagAlgo(arry, size);
   printArray(arry, size);
 }
